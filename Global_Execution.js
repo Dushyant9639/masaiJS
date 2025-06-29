71750f8e3b6cd7c4c7c8119123cebc59f5c788ee
Dushyant9639 <goyaldushyant075@gmail.com>
2025-06-29 21:50:42 +0530

    Add files via upload

---
+++ Global_Execution.js
@@ -0,0 +1,11 @@
+var age= 25;
+function displayAge (){
+    console.log("Age is", age)
+}
+function changeAge (){
+age= 30
+console.log("Updated age is", age)
+}
+displayAge()
+changeAge()
+displayAge()
