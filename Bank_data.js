860662fda7ed2e1ced2ac5fc67323e7e2b0a7df6
Dushyant9639 <goyaldushyant075@gmail.com>
2025-06-30 22:45:20 +0530

    Add files via upload

---
+++ Bank_data.js
@@ -0,0 +1,33 @@
+function createBankAccount(initialBalance) {
+    let balance = initialBalance; 
+
+    return {
+        deposit: function(amount) {
+            if (amount <= 0) {
+                return "Deposit amount must be greater than zero.";
+            }
+            balance += amount;
+            return balance;
+        },
+        withdraw: function(amount) {
+            if (amount > balance) {
+                return "Insufficient funds.";
+            }
+            if (amount <= 0) {
+                return "Withdrawal amount must be greater than zero.";
+            }
+            balance -= amount;
+            return balance;
+        },
+        getBalance: function() {
+            return balance;
+        }
+    };
+}
+const account = createBankAccount(100);
+
+console.log(account.deposit(50)); 
+
+console.log(account.withdraw(30)); 
+
+console.log(account.getBalance()); 
