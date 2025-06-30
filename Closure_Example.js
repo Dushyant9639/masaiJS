931f717ce1913fbc29305c8ac8270315a9e94e09
Dushyant9639 <goyaldushyant075@gmail.com>
2025-06-30 22:32:33 +0530

    Add files via upload

---
+++ Closure_Example.js
@@ -0,0 +1,9 @@
+function outerFunction() {
+    const message = "Hello from the closure!";
+    
+    return function innerFunction() {
+        console.log(message);
+    };
+}
+const storedFunction = outerFunction();
+storedFunction(); 
