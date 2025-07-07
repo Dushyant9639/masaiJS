93362ffe641ecbc45a90bc41d0de6a4298ad9715
Dushyant9639 <goyaldushyant075@gmail.com>
2025-07-07 22:19:38 +0530

    Add files via upload

---
+++ Fetchdata.js
@@ -0,0 +1,21 @@
+function fetchData() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (Math.random() > 0.5) {
+        resolve("Fetched data successfully!");
+      } else {
+        reject("Network error or server failure.");
+      }
+    }, 1000); 
+  });
+}
+async function fetchDataHandler() {
+  try {
+    const result = await fetchData();
+    console.log(result);
+  } catch (error) {
+    console.log("Error fetching data:", error);
+  }
+}
+fetchDataHandler();
+
